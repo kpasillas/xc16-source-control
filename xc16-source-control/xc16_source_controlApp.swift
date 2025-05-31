0ce00ec92a28f66472af91c1069345916f8374a9
Kristopher Pasillas <kris@kris-p.net>
2025-05-31 14:15:00 -0700

    Initial Commit

---
+++ xc16-source-control/xc16_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  xc16_source_controlApp.swift
+//  xc16-source-control
+//
+//  Created by Kristopher Pasillas on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct xc16_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xc16-source-controlTests/xc16_source_controlTests.swift
@@ -0,0 +1,16 @@
+//
+//  xc16_source_controlTests.swift
+//  xc16-source-controlTests
+//
+//  Created by Kristopher Pasillas on 5/31/25.
+//
+
+import Testing
+
+struct xc16_source_controlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
